fd912d1b0c644bbbe9c78f26da3131ea7bf27057
Jay <snootched@gmail.com>
2026-09-07 15:56:05 -0400

    dev release 2026.09.0-dev1

--- custom_components/lcards/const.py
+++ custom_components/lcards/const.py
@@ -3,7 +3,7 @@ import logging
 
 DOMAIN = "lcards"
 FRONTEND_SCRIPT_URL = "lcards.js"
-DOMAIN_VERSION = "2026.09.0-dev0"
+DOMAIN_VERSION = "2026.09.0-dev1"
 
 # Options
 CONF_SHOW_PANEL = "show_panel"
